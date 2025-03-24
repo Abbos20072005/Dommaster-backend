387335498d7386bddb0918041a3940249de72412
abbosbek <hikmatullaevabbos24@gmail.com>
2025-03-24 11:53:03 -0400

    banner api added

--- config/urls.py
+++ config/urls.py
@@ -25,9 +25,10 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/v1/auth/', include("authorization.urls")),
-    path('api/v1/', include("service.urls")),
+    path("admin/", admin.site.urls),
+    path("api/v1/auth/", include("authorization.urls")),
+    path("api/v1/base/", include("base.urls")),
+    path("api/v1/", include("service.urls")),
 
     re_path(r'static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
     re_path(r'media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
