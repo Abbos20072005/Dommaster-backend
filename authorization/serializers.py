1d304597557d0ed84251d257285a87907b95013d
abbosbek <hikmatullaevabbos24@gmail.com>
2025-04-03 19:08:24 -0400

    password hashing is added while creating the user

--- authorization/serializers.py
+++ authorization/serializers.py
@@ -1,12 +1,14 @@
 from .models import Customer
 from rest_framework import serializers
 from .utils import validate_number
+from django.contrib.auth.hashers import make_password
 
 
 class LoginSerializer(serializers.Serializer):
     email = serializers.EmailField(required=False)
     phone_number = serializers.CharField(max_length=14, required=False, validators=[validate_number])
     password = serializers.CharField(max_length=30, required=False)
+
 
 class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
@@ -15,6 +17,11 @@ class RegisterSerializer(serializers.ModelSerializer):
         extra_kwargs = {
             'password': {'write_only': True}
         }
+
+    def create(self, validated_data):
+        validated_data["password"] = make_password(validated_data["password"])
+        return super().create(validated_data)
+
 
 class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
