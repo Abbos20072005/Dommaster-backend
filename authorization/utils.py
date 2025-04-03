ed95b72b6778fc9cd207687b467db3f2b46f7712
abbosbek <hikmatullaevabbos24@gmail.com>
2025-04-03 19:08:53 -0400

    Uzbek number validation added

--- authorization/utils.py
+++ authorization/utils.py
@@ -3,6 +3,6 @@ from django.core.validators import ValidationError
 
 def validate_number(value):
     if not re.match(r'^\+998\d{9}$', value):
-        raise ValidationError("Iltimos O'zbekiston telefon raqamini kiriting!")
+        raise ValidationError("Please enter uzbek number")
 
     return value
