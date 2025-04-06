a4628221f0bfc84a28c35eda6ad1eae94ba61441
abbosbek <hikmatullaevabbos24@gmail.com>
2025-04-06 01:50:23 -0400

    new error messages added to exceptions

--- exceptions/error_messages.py
+++ exceptions/error_messages.py
@@ -13,7 +13,14 @@ class ErrorCodes(Enum):
     INCORRECT_PASSWORD = 8
     USER_BLOCKED = 9
     ATTEMPT_ALREADY_EXISTS = 10
-
+    OTP_KEY_NOT_FOUND = 11
+    INCORRECT_OTP = 12
+    OTP_EXPIRED = 13
+    OTP_ATTEMPTS_LIMITE = 14
+    INVALID_TOKEN = 15
+    OTP_NOT_EXPIRED = 16
+    NEW_PASSWORD_NOT_MATCH = 17
+    OLD_PASSWORD_NOT_MATCH = 18
 
 
 error_messages = {
@@ -27,9 +34,19 @@ error_messages = {
     8: {"result": "Incorrect password", "http_status": status.HTTP_400_BAD_REQUEST},
     9: {"result": "User Blocked, Contact admins", "http_status": status.HTTP_400_BAD_REQUEST},
     10: {"result": "You already have 3 attempts, please return after 12 times",
-        "http_status": status.HTTP_400_BAD_REQUEST},
+         "http_status": status.HTTP_400_BAD_REQUEST},
+    11: {"result": "otp_key does not exist", "http_status": status.HTTP_400_BAD_REQUEST},
+    12: {"result": "incorrect otp_code", "http_status": status.HTTP_400_BAD_REQUEST},
+    13: {"result": "OTP key Expired", "http_status": status.HTTP_400_BAD_REQUEST},
+    14: {"result": "OTP attempt riched it's limit", "http_status": status.HTTP_400_BAD_REQUEST},
+    15: {"result": "Invalid Token", "http_status": status.HTTP_400_BAD_REQUEST},
+    16: {"result": "Otp not expired", "http_status": status.HTTP_400_BAD_REQUEST},
+    17: {"result": "New password and confirming new passoword fields are not match",
+         "http_status": status.HTTP_400_BAD_REQUEST},
+    18: {"result": "Old password not match your current password", "http_status": status.HTTP_400_BAD_REQUEST}
 
 }
+
 
 def get_error_message(code):
     return error_messages.get(code, 'Unknown error')
