3912f1f84f7a178e7faeeaa297d55a2731d296b4
abbosbek <hikmatullaevabbos24@gmail.com>
2025-04-07 01:23:58 -0400

    urls added

--- service/urls.py
+++ service/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import ProductViewSet
+from .views import ProductViewSet, BrandViewSet, SaleViewSet
 
 urlpatterns = [
     path("categories/", ProductViewSet.as_view({"get": "categories_list"}), name="categories list"),
@@ -7,5 +7,10 @@ urlpatterns = [
     path("item/categories/<int:pk>/", ProductViewSet.as_view({"get": "item_category_list"}),
          name="item categories list"),
     path("categories/products/<int:pk>/", ProductViewSet.as_view({"get": "categories_product_list"}),
-         name="categories product list")
+         name="categories product list"),
+    path("products/<int:pk>/", ProductViewSet.as_view({"get": "product_detail"}), name="products detail"),
+    path("brands/", BrandViewSet.as_view({"get": "brand_list"}), name="brand list"),
+    path("brands/<int:pk>/", BrandViewSet.as_view({"get": "brand_detail"}), name="brand detail"),
+    path("brands/products/<int:pk>/", BrandViewSet.as_view({"get": "brand_products"}), name="brand products"),
+    path("sale/", SaleViewSet.as_view({"get": "sale_products"}), name="sale products")
 ]
