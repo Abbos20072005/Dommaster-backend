7471869e7cc5db5daab884949456808186abc7cf
abbosbek <hikmatullaevabbos24@gmail.com>
2025-04-09 22:59:18 -0400

    Customer model phone number and email uniqness changed

--- authorization/models.py
+++ authorization/models.py
@@ -6,9 +6,9 @@ import uuid
 
 class Customer(BaseModel):
     full_name = models.CharField(max_length=150, verbose_name="Полное имя")
-    phone_number = models.CharField(max_length=14, unique=True, validators=[validate_number],
+    phone_number = models.CharField(max_length=14, validators=[validate_number],
                                     verbose_name="Номер телефона")
-    email = models.EmailField(verbose_name="Электронная почта", unique=True)
+    email = models.EmailField(verbose_name="Электронная почта")
     password = models.CharField(verbose_name="Пароль")
     verified = models.BooleanField(default=False, verbose_name="Подтвержден")
 
