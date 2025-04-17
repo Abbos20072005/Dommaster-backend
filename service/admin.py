47ad911397425feb590d9d45cd8729b72fc7a0fa
abbosbek <hikmatullaevabbos24@gmail.com>
2025-04-17 19:22:18 -0400

    cart and cart item registered to admin panel

--- service/admin.py
+++ service/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
 from .models import Product, ProductCategory, ProductSubCategory, ProductItemCategory, Comment, Order, \
-    OrderItem, Tag, Brand, Sale, AddsBrands, ProductImage, Favourites
+    OrderItem, Tag, Brand, Sale, AddsBrands, ProductImage, Favourites, Cart, CartItem
+
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ("id", "customer")
+    list_display_links = ("id", "customer")
+
+
+@admin.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ("id", "cart", "product")
+    list_display_links = ("id", "cart")
 
 
 @admin.register(Favourites)
