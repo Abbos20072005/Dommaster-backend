a2f52a3805c604eaeb010442aed0c4d9ffe400c2
abbosbek <hikmatullaevabbos24@gmail.com>
2025-04-17 19:24:54 -0400

    urls updated

--- service/urls.py
+++ service/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import ProductViewSet, BrandViewSet, SaleViewSet, AddsBrandsViewSet, CommentViewSet, FavouriteViewSet
+from .views import ProductViewSet, BrandViewSet, SaleViewSet, AddsBrandsViewSet, CommentViewSet, FavouriteViewSet, \
+    CartViewSet
 
 urlpatterns = [
     path("categories/", ProductViewSet.as_view({"get": "categories_list"}), name="categories list"),
@@ -19,6 +20,9 @@ urlpatterns = [
     path("comment/delete/<int:pk>/", CommentViewSet.as_view({"delete": "comment_delete"}), name="comment delete"),
     path("most/sold/", ProductViewSet.as_view({"get": "most_sold"}), name="most sold"),
     path("favourite/create/", FavouriteViewSet.as_view({"post": "create_favourite"}), name="create favourite"),
-    path("favourite/list/", FavouriteViewSet.as_view({"get": "favourite_list"}), name="favourite list")
+    path("favourite/list/", FavouriteViewSet.as_view({"get": "favourite_list"}), name="favourite list"),
+    path("cart/", CartViewSet.as_view({"get": "get_cart"}), name="get cart"),
+    path("cart/item/create/", CartViewSet.as_view({"post": "create_cart_item"}), name="create cart item"),
+    path("cart/item/update/", CartViewSet.as_view({"patch": "update_cart_item"}), name="update cart item")
 
 ]
