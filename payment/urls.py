9dda2900b458fa8698d966b5fdcbefd67ae61bd3
suxrob <suxrobergashev2102@gmail.com>
2025-04-18 16:27:18 +0500

    payment url change

--- payment/urls.py
+++ payment/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import PreparePaymentView, CompletePaymentView
 
 urlpatterns = [
-    path('/prepare/', PreparePaymentView.as_view()),
-    path('/complete/', CompletePaymentView.as_view()),
+    path('prepare/', PreparePaymentView.as_view()),
+    path('complete/', CompletePaymentView.as_view()),
 ]
