ea072a5cf0e8591a4fb9f98fa141e9fce1d7553d
abbosbek <hikmatullaevabbos24@gmail.com>
2025-04-20 01:58:38 -0400

    address crud api changed

--- authorization/urls.py
+++ authorization/urls.py
@@ -10,8 +10,6 @@ urlpatterns = [
     path("otp/verify/", OTPViewSet.as_view({"post": "otp_verify"}), name="otp verify"),
     path("otp/resend/", OTPViewSet.as_view({"post": "otp_resend"}), name="otp resend"),
     path("customer/update/", AuthViewSet.as_view({"patch": "update_customer_info"}), name="update customer info"),
-    path("customer/addresses/", AuthViewSet.as_view({"get": "addresses_list"}), name="addresses list"),
-    path("customer/addresses/", AuthViewSet.as_view({"post": "address_create"}), name="address create"),
-    path("customer/addresses/<int:pk>/", AuthViewSet.as_view({"patch": "addresses_update"}), name="address update"),
-    path("customer/addresses/<int:pk>/", AuthViewSet.as_view({"delete": "delete_address"}), name="delete address")
+    path("customer/addresses/", AuthViewSet.as_view({"get": "addresses_list", "post": "address_create"}), name="addresses list"),
+    path("customer/addresses/<int:pk>/", AuthViewSet.as_view({"patch": "addresses_update", "delete": "delete_address"}), name="address update"),
 ]
