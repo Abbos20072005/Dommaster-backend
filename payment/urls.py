812d7fbea3aa1330a826573c897fa8c5e0c777b9
suxrob <suxrobergashev2102@gmail.com>
2025-04-21 13:56:21 +0500

    payme urals added

--- payment/urls.py
+++ payment/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import PreparePaymentView, CompletePaymentView
+from .views import PreparePaymentView, CompletePaymentView, MerchantAPIView
 
 urlpatterns = [
     path('prepare/', PreparePaymentView.as_view()),
     path('complete/', CompletePaymentView.as_view()),
+    path('', MerchantAPIView.as_view()),
 ]
