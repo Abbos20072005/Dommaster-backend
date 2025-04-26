0a4bae6b13b726cd3f8849e5f32f28facd7ed859
abbosbek <hikmatullaevabbos24@gmail.com>
2025-04-26 00:25:58 -0400

    Chat added to admin panel

--- base/admin.py
+++ base/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from .models import Banner, Chat, AboutUs, Messages, Promocodes
 
+
+@admin.register(Chat)
+class ChatAdmin(admin.ModelAdmin):
+    list_display = ("id", "customer")
+    list_display_links = ("id", "customer")
+
+
 @admin.register(Promocodes)
 class PromocodesAdmin(admin.ModelAdmin):
     list_display = ("id", "name", "expires_at")
@@ -11,11 +18,13 @@ class PromocodesAdmin(admin.ModelAdmin):
         obj.name = obj.name.lower()
         obj.save()
 
+
 @admin.register(Banner)
 class BannerAdmin(admin.ModelAdmin):
     list_display = ('id', 'title')
     list_display_links = ('id', 'title')
     search_fields = ('title', 'short_description')
+
 
 @admin.register(Messages)
 class ChatAdmin(admin.ModelAdmin):
@@ -24,10 +33,10 @@ class ChatAdmin(admin.ModelAdmin):
     list_filter = ("is_answer",)
     readonly_fields = ("is_answer",)
 
-
     def save_model(self, request, obj, form, change):
         obj.is_answer = True
         obj.save()
+
 
 @admin.register(AboutUs)
 class AboutUsAdmin(admin.ModelAdmin):
