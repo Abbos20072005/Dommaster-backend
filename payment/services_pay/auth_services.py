90439888c588095a738315a44902b46dce651ef7
abbosbek <hikmatullaevabbos24@gmail.com>
2026-04-19 23:47:13 +0500

    atmos callback changed

--- payment/services_pay/auth_services.py
+++ payment/services_pay/auth_services.py
@@ -484,8 +484,8 @@ class AtmosHoldService:
             json=json_data,
             timeout=5,
         )
-        res.raise_for_status()
         print(res.raise_for_status())
+        res.raise_for_status()
         print(res.json())
         data = res.json()
         print(data)
