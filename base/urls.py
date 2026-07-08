287481fb073a556b68cf2e367b7b94d791c87799
abbosbek <hikmatullaevabbos24@gmail.com>
2026-07-08 11:00:00 +0500

    base informations api added

--- base/urls.py
+++ base/urls.py
@@ -17,5 +17,5 @@ urlpatterns = [
     path("video/", VideoViewSet.as_view({"get": "video_list"}), name="video_list"),
     path("video/<int:pk>/", VideoViewSet.as_view({"get": "video_detail"}), name="video_detail"),
     path("delete-button/", DeleteButtonViewSet.as_view({"get": "delete_button"}), name="delete_button"),
-    path("base-info/", BaseInformationViewSet.as_view({"get": "base_info"}), name="base_info"),
+    path("info/", BaseInformationViewSet.as_view({"get": "base_info"}), name="base_info"),
 ]
